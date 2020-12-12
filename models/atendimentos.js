85d4c55335f8125274d0a10c6b233a706eaa5064
Laurivan <laurivansareta@gmail.com>
2020-12-12 17:10:01 -0300

    Aula 03

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -1,6 +1,7 @@
 const conexao = require('../infraestrutura/conexao')
 const moment = require('moment')
 const utilModels = require('./util.models')
+const axios = require('axios')
 
 class Atendimento {
     adiciona(atendimento, res) {
@@ -10,7 +11,7 @@ class Atendimento {
         let {cliente, pet, servico, status, observacoes, dataAgenamento} = atendimento
         const dataCriacao = moment().format('YYYY-MM-DD HH:mm:ss')
         dataAgenamento = moment(atendimento.data_agendamento, 'DD/MM/YYYY HH:mm').format('YYYY-MM-DD HH:mm:ss')
-        
+
         const dataEhValida = moment(dataAgenamento).isSameOrAfter(dataCriacao)
         const clienteEhValido = cliente.length >= 5
 
@@ -62,11 +63,15 @@ class Atendimento {
     buscaPorId(id, res){
         const sql = 'SELECT * FROM atendimentos WHERE id = $1 ;'
 
-        conexao.query(sql, [id], (erro, resultados) => {
+        conexao.query(sql, [id], async (erro, resultados) => {
             const atendimento = resultados.rows[0]
+            const cpf = atendimento.cliente
+
             if (erro) {
                 res.status(400).json(erro)
             } else {
+                const { data } = await axios.get('http://localhost:8082/' + cpf)
+                atendimento.cliente = data
                 res.status(200).json(atendimento)
             }
         })
