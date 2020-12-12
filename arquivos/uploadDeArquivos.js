7338bd3775a23146e2fe43cdaf92c5968933b4e2
Laurivan <laurivansareta@gmail.com>
2020-12-12 16:23:23 -0300

    Aula 02.02

--- arquivos/uploadDeArquivos.js
+++ arquivos/uploadDeArquivos.js
@@ -1,5 +1,9 @@
 const fs = require('fs')
 
-fs.createReadStream('./assets/salsicha.jpg')
-    .pipe(fs.createWriteStream('./assets/salsicha-stream.jpg'))
-    .on('finish', () => console.log('Imagem escrita com sucesso'))
+module.exports = (caminho, nomeDoArquivo, callbackImagemCriada) => {
+    const novoCaminho = './assets/imagens/' + nomeDoArquivo
+
+    fs.createReadStream(caminho)
+    .pipe(fs.createWriteStream(novoCaminho))
+    .on('finish', () => callbackImagemCriada(novoCaminho))
+}
--- models/pets.js
+++ models/pets.js
@@ -1,17 +1,23 @@
 const conexao = require('../infraestrutura/conexao')
 const utilModels = require('./util.models')
+const uploadDeArquivo = require('../arquivos/uploadDeArquivos')
 
 class Pet {
     adiciona(pet, res) {
-        const consulta = utilModels.createInsertQuery('pets', pet)
         
-        conexao.query(consulta.sql, consulta.params, (erro, resultado) => {
-            if (erro) {
-                res.status(400).json(erro)
-            } else {
-                const id = resultado.rows[0].id
-                res.status(200).json({...pet, id})
-            }
+
+        uploadDeArquivo(pet.imagem, pet.nome, novoCaminho => {            
+            const novoPet = {nome: pet.nome, imagem: novoCaminho}
+            const consulta = utilModels.createInsertQuery('pets', novoPet)
+
+            conexao.query(consulta.sql, consulta.params, (erro, resultado) => {
+                if (erro) {
+                    res.status(400).json(erro)
+                } else {
+                    const id = resultado.rows[0].id
+                    res.status(200).json({...novoPet, id})
+                }
+            })
         })
     }
 }
